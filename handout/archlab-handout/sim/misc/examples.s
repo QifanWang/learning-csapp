cc7a8243d696b89b30cf7942ec929b0ee5bb9fbf
QifanWang <302755067@qq.com>
2021-05-23 21:45:51 +0800

    Part A of Arch Lab

---
+++ handout/archlab-handout/sim/misc/examples.s
@@ -0,0 +1,127 @@
+	.file	"examples.c"
+	.text
+	.globl	sum_list
+	.type	sum_list, @function
+sum_list:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movq	$0, -8(%rbp)
+	jmp	.L2
+.L3:
+	movq	-24(%rbp), %rax
+	movq	(%rax), %rax
+	addq	%rax, -8(%rbp)
+	movq	-24(%rbp), %rax
+	movq	8(%rax), %rax
+	movq	%rax, -24(%rbp)
+.L2:
+	cmpq	$0, -24(%rbp)
+	jne	.L3
+	movq	-8(%rbp), %rax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sum_list, .-sum_list
+	.globl	rsum_list
+	.type	rsum_list, @function
+rsum_list:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)
+	cmpq	$0, -24(%rbp)
+	jne	.L6
+	movl	$0, %eax
+	jmp	.L7
+.L6:
+	movq	-24(%rbp), %rax
+	movq	(%rax), %rax
+	movq	%rax, -16(%rbp)
+	movq	-24(%rbp), %rax
+	movq	8(%rax), %rax
+	movq	%rax, %rdi
+	call	rsum_list
+	movq	%rax, -8(%rbp)
+	movq	-16(%rbp), %rdx
+	movq	-8(%rbp), %rax
+	addq	%rdx, %rax
+.L7:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	rsum_list, .-rsum_list
+	.globl	copy_block
+	.type	copy_block, @function
+copy_block:
+.LFB2:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	%rdx, -40(%rbp)
+	movq	$0, -16(%rbp)
+	jmp	.L9
+.L10:
+	movq	-24(%rbp), %rax
+	leaq	8(%rax), %rdx
+	movq	%rdx, -24(%rbp)
+	movq	(%rax), %rax
+	movq	%rax, -8(%rbp)
+	movq	-32(%rbp), %rax
+	leaq	8(%rax), %rdx
+	movq	%rdx, -32(%rbp)
+	movq	-8(%rbp), %rdx
+	movq	%rdx, (%rax)
+	movq	-8(%rbp), %rax
+	xorq	%rax, -16(%rbp)
+	subq	$1, -40(%rbp)
+.L9:
+	cmpq	$0, -40(%rbp)
+	jg	.L10
+	movq	-16(%rbp), %rax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	copy_block, .-copy_block
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
